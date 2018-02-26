4046b1ca2d1de85bb28129ea3c790933dd7080c8
christian_yvan <christian.yvan@gmail.com>
2018-02-26 02:32:49 +0100

    Ajout commentaire

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
  * index.php fera office de controlleur frontal, à partir duquel le controlleur 
  * associé à l'action de l'utilisateur via le router sera utilisé.
  */
-require_once 'controllers/Router.php';
 
+define('URL', str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https":"http")."://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+require_once 'controllers/Router.php';
 $router = new Router();
 $router->routeReq();
