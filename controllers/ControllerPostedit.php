18c2cfa569d7185b17a6b1da682b4a540a114305
christian_yvan <christian.yvan@gmail.com>
2018-03-02 17:56:33 +0100

    modification commentaires

--- controllers/ControllerPostedit.php
+++ controllers/ControllerPostedit.php
@@ -1,17 +1,82 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of ControllerPostedit
- *
+ * Description of ControllerEditpost
+ * Classe qui permet de modifier un post , cela se fait à partir d'un formulaire 
+ * dans lequel on retrouve les éléments du post à modifier.
  * @author Christian
  */
-class ControllerPostedit
-{
-    //put your code here
+
+class ControllerPostedit {
+   
+    private $_view;
+    private $_postManager;
+   
+    public function __construct($action)
+    {
+        if (isset($action) && count($action) > 1) 
+        {
+            throw new Exception('Page introuvable');
+        }   else 
+            {
+                if(isset($_POST['publish']))
+                {
+                    $title = htmlspecialchars(trim($_POST['title']));
+                    $content = htmlspecialchars(trim($_POST['content']));
+                    $posted = isset($_POST['public'])? 1 : 0;
+                        
+                    $errors = [];
+                    if(empty($title)|| empty($content))
+                    {
+                        $errors['empty']= "Veuillez remplir tout les champs...";
+                    }
+                    
+                }        
+                
+                if(!empty($errors))
+                {
+                    $this->_view = new View('Error');
+                    $this->_view->generate(array('errors' => $errors));
+                                
+                }   
+                else
+                {
+                    $this->edit($_GET['id'],$title,$content,$posted);
+                }    
+            }
+    }
+    
+   
+    /**
+     * Fonction qui prend en paramètre les données de l'article modifié et le 
+     * modifie dans la base de donnée.
+     * @param type $id
+     * @param type $title
+     * @param type $content
+     * @param type $posted
+     */
+    private function edit($id,$title,$content,$posted)
+    {
+        $this->_postManager = new PostManager();
+        $this->_postManager-> updatePost($id,$title,$content,$posted);
+       
+        $post = $this->_postManager->getAllPost($id);
+        
+        if($post->content() == null)
+        {
+            $errors = [];
+            $errors['identity'] = "Pas de post pour cet identifiant";      
+                    $this->_view = new View('Error');
+                    $this->_view->generate(array('errors' => $errors));
+        }   else
+            {
+                $this->_view = new View('Postadmin');
+                $this->_view->generate(array('post' => $post));
+            }
+    }
 }
+ 
+   
+    
+
+
