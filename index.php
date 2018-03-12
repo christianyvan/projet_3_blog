08ec80b059f5a28e1a5dad858bf591686a51b49b
christian_yvan <christian.yvan@gmail.com>
2018-03-12 12:41:35 +0100

    suppression définition URL

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
  * associé à l'action de l'utilisateur via le router sera utilisé.
  */
 
-define('URL', str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https":"http")."://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
 require_once 'controllers/Router.php';
 $router = new Router();
 $router->routeReq();
