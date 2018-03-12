d5eb6069287708e01a15774eca143c60166d6aab
christian_yvan <christian.yvan@gmail.com>
2018-03-12 11:01:51 +0100

    modification barre navigation

--- views/template.php
+++ views/template.php
@@ -29,61 +29,53 @@ $action = str_replace("/index.php?action=", "",$action);
 
     <body class="background blue lighten-5">
         <div class=" navbar-fixed">     
-            <nav class="black container-fluid">
-                <div class="wrapper container">
-                    <a href="index.php?action=home" class="brand-logo">Blog de Jean Forteroche</a>
-                    <a href="#" data-activates="smartphone-menu" class="button-collapse"><i class="material-icons">menu</i></a>
+            <nav class="black">
+                <div class="container">
+                    <div class="wrapper container">
+                        <a href="index.php?action=home" class="brand-logo">Blog de Jean Forteroche</a>
+                        <a href="#" data-activates="smartphone-menu" class="button-collapse"><i class="material-icons">menu</i></a>
         
-                    <ul class="right hide-on-med-and-down" >
-                        <li>
-                            <a class="<?=($action=="home")?"background grey":"";?>" href="index.php?action=home">Accueil</a>
-                        </li>
+                        <ul class="right hide-on-med-and-down" >
+                            <li>
+                                <a class="<?=($action=="home")?"background grey":"";?>" href="index.php?action=home">Accueil</a>
+                            </li>
                         
-                        <li >
-                            <a class="<?=($action=="blog")?"background grey":"";?>" href="index.php?action=blog">Blog</a>
-                        </li>
+                            <li >
+                                <a class="<?=($action=="blog")?"background grey":"";?>" href="index.php?action=blog">Sommaire</a>
+                            </li>
                 
-                        <li>
-                            <a  class="<?=($action=="login")?"background grey":"";?>" href="index.php?action=login">Admin</a>
-                        </li>
+                            <li>
+                                <a  class="<?=($action=="login")?"background grey":"";?>" href="index.php?action=login">Tableau de bord</a>
+                            </li>
                         
-                        <li>
-                            <a  class="<?=($action=="inscription")?"background grey":"";?>" href="index.php?action=inscription">Inscription</a>
-                        </li>
-                    </ul>
+                            <li>
+                                <a  class="<?=($action=="inscription")?"background grey":"";?>" href="index.php?action=inscription">Inscription</a>
+                            </li>
+                        </ul>
         
-                    <ul class="side-nav" id="smartphone-menu">
-                        <li class="<?php echo($action=="home")?"active":"";?>">
-                            <a  href="index.php?action=home">Accueil</a>
-                        </li>
+                        <ul class="side-nav" id="smartphone-menu">
+                            <li class="<?php echo($action=="home")?"active":"";?>">
+                                <a  href="index.php?action=home">Accueil</a>
+                            </li>
                         
-                        <li class="<?php echo($action=="blog")?"active":"";?>">
-                            <a href="index.php?action=blog">Sommaire</a>
-                        </li>
+                            <li class="<?php echo($action=="blog")?"active":"";?>">
+                                <a href="index.php?action=blog">Sommaire</a>
+                            </li>
                 
-                        <li class="<?php echo($action=="login")?"active":"";?>">
-                            <a  href="index.php?action=login">Tableau de bord</a>
-                        </li>
+                            <li class="<?php echo($action=="login")?"active":"";?>">
+                                <a  href="index.php?action=login">Tableau de bord</a>
+                            </li>
                         
-                        <li class="<?php echo($action=="inscription")?"active":"";?>">
-                            <a  href="index.php?action=inscription">Inscription</a>
-                        </li>
-                    </ul>
+                            <li class="<?php echo($action=="inscription")?"active":"";?>">
+                                <a  href="index.php?action=inscription">Inscription</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>    
         </div>     
-        
-        <div class="container-fluid" style="padding-top:20px">
-            <div class="card-content background">
-                <div class="card-title container background ">
-                    <br/>
-                    <h3 id="h11"><a  href="<?= URL ?>">Bienvenu sur le blog de Jean Forteroche</a></h3>
-                </div>
-                <p><?= $content ?></p>
-            </div>
-        </div><!-- /.container -->
-        
-    
+        <p><?= $content ?></p>
+                        
     <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.js"></script>
