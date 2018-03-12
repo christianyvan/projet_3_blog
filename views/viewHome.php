6038604ea749194192694fe014efda438d5f61a2
christian_yvan <christian.yvan@gmail.com>
2018-03-12 14:02:22 +0100

    modification card-content

--- views/viewHome.php
+++ views/viewHome.php
@@ -6,7 +6,7 @@
 <br/><br/>
 <hr>
 <div class="container">
-    <h3 id="h31">Derniers chapitres publiés</h3>
+    <h3 id="h31" class="left grey-text">Derniers chapitres publiés</h3>
 </div>
 <br/><br/>
 
@@ -16,10 +16,7 @@
     <?php       
     foreach ($posts as $post): ?>
         <div class=" col l6 m6 s12">
-        
-        
             <div class="card">
-            
                 <div class="card-content">
                      <h5 class="grey-text text-darken-2"><?= $post->title();?></h5>
                     <h6 class="grey-text text-darken-2"><time>Le<?= date(" d/m/Y à H:i",strtotime($post->date()));?></time> par <?= $post->name(); ?></h6>
@@ -30,21 +27,16 @@
                 </div>
             
                 <div class="card-content">
-                    <div class="row">
-                    <span class="col l2 card-title activator right grey-text text-darken-4 "><i class="material-icons">more_vert</i></span>
-                
-                        <div class="col l5"><a href="index.php?action=post&id=<?= $post->id();?>">Article complet</a></div>
-                        <div class="col l5"><a href="index.php?action=blog">Voir plus</a></div>
+                    <div class="row s12 l6 m6">
+			<span class="card-title activator grey-text offset-l1 col l3  ">LIRE</span>
+			<p><a class="grey-text  offset-l1 col l3 center" href="index.php?action=post&id=<?= $post->id();?>">Commenter</a></p>
+			<p><a class="grey-text offset-l1 col l3  center" href="index.php?action=blog">Sommaire</a></span>
                     </div>
-                </div>
+		</div>  
             
                 <div class="card-reveal">
                     <span class="card-title grey-text text-darken-4"><?= $post->title();?><i class="material-icons right">close</i></span>
-                        <?php $content =$post->content();
-                        $content = htmlspecialchars_decode($content);
-                        $content = substr($content,0,1500);
-                        ?>
-                <p><?= $content ?>...</p>
+                <p><?= $post->content()?>...</p>
                 </div>
             </div>
         </div>
