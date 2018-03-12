dc0f87af3765a0566eb3422fc87bfad133d2d49c
christian_yvan <christian.yvan@gmail.com>
2018-03-12 10:26:54 +0100

    finalisation du code de la vue

--- views/viewError.php
+++ views/viewError.php
@@ -1,3 +1,17 @@
-<?php
+<!-- Vue qui affichera les erreurs -->
+<?php ob_start()?>
+ <div class="container">   
+    <div class="card red">
+        <div class="card-content white-text">
+            <?php
+                foreach($errors as $error){
+                    echo '<h3>'.$error.'</h3><br/>'; 
+               }
+            ?>
+        </div>
+    </div>
+</div>
+<?php $content = ob_get_clean();?>                                  
+<?php require 'template.php';?>
 
 
